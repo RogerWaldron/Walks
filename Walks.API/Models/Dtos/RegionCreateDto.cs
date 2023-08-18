059c7ddf952e3022f8c9843c63c9854393f62e1c
rwaldron <outsideinprofits@gmail.com>
2023-08-18 17:51:02 +1000

    added RegionDtos to Create and Update

---
+++ Walks.API/Models/Dtos/RegionCreateDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Walks.API.Models.Dtos
+{
+	public class RegionCreateDto
+	{
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string? RegionImgUrl { get; set; }
+    }
+}
+
