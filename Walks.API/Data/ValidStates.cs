697b630a9561b030e2db88d7e85ddb849a8069a7
rwaldron <outsideinprofits@gmail.com>
2023-08-20 11:02:12 +1000

    Created ValidStates enum to centralise state change messages

---
+++ Walks.API/Data/ValidStates.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Walks.API.Data
+{
+	public enum ValidStates
+	{
+        NotFound = 0,
+        Duplicate = 1,
+        Repository = 2,
+        SoftDeleted = 3,
+        HardDeleted = 4,
+        Created = 5,
+        Updated = 6,
+        OK = 7,
+        Error = 8,
+        Exists = 9,
+    }
+}
+
