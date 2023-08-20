040e01182a347aedea28e0c81f41554be31f814f
rwaldron <outsideinprofits@gmail.com>
2023-08-20 11:09:02 +1000

    Added AutoMapper and Mapping Profiles

--- Walks.API/Program.cs
+++ Walks.API/Program.cs
@@ -4,6 +4,8 @@ using Walks.API.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
