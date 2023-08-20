ce9225bbc0e5ba70d42038a6423b63c7bbb3f2e0
rwaldron <outsideinprofits@gmail.com>
2023-08-20 12:14:49 +1000

    Fix typo in model

--- Walks.API/Models/Dtos/RegionCreateDto.cs
+++ Walks.API/Models/Dtos/RegionCreateDto.cs
@@ -8,7 +8,7 @@ namespace Walks.API.Models.Dtos
         [MinLength(2, ErrorMessage = "Region name can not be less than two characters")]
         [MaxLength(150, ErrorMessage = "Region name to long")]
         public string RegionName { get; set; }
-        public string Code { get; set; };
+        public string Code { get; set; }
         public string? RegionImgUrl { get; set; }
         public bool IsClosed { get; set; }
     }
