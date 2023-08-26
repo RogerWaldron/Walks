ef9b6b079a0f4a03a0d9c343402b0bafc4413894
rwaldron <outsideinprofits@gmail.com>
2023-08-26 11:59:53 +1000

    New Migration for new UpdatedOn field

---
+++ Walks.API/Migrations/20230825074455_WalkCreateDto.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Walks.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class WalkCreateDto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
